aadb2f7b39d0e12a0a9439339c3d2fed3f44d924
ineternet <kevinsim123123@gmail.com>
2022-05-28 00:22:03 +0200

    little cleanup

--- src/server/init.server.lua
+++ src/server/init.server.lua
@@ -2,24 +2,22 @@ local units = require(game.ReplicatedStorage.Common.ResourceUnit)
 local items = require(game.ReplicatedStorage.Common.Item)
 local const = require(game.ReplicatedStorage.Common.Const)
 
-local tu = units.new()
+local char = units.new()
 
-tu.charsheet.class = const.Classes.Mage
-tu.charsheet.race = const.Races.Werebeast
-tu.charsheet.level = 16
+char.charsheet.class = const.Classes.Mage
+char.charsheet.race = const.Races.Werebeast
+char.charsheet.level = 16
 
 local stamdagger = items.newOf(const.Items.StamDagger)
 
-local _ = tu.charsheet.equipment:swap(const.Slots.MainHand, stamdagger)
+local _ = char.charsheet.equipment:swap(const.Slots.MainHand, stamdagger)
 
 
 print(("Level %d %s %s %s has %d Spell Power. Equipment: "):format(
-    tu.charsheet.level,
-    const.Specs[tu.charsheet.spec],
-    const.Classes[tu.charsheet.class],
-    const.Races[tu.charsheet.race],
-    tu.charsheet:spellPower()
+    char.charsheet.level,
+    const.Specs[char.charsheet.spec],
+    const.Classes[char.charsheet.class],
+    const.Races[char.charsheet.race],
+    char.charsheet:spellPower()
 ))
-print(tu.charsheet.equipment)
-
---> 5
+print(char.charsheet.equipment)
--- src/shared/Equipment.lua
+++ src/shared/Equipment.lua
@@ -7,24 +7,24 @@ local Equipment = use"Object".inherit"Equipment"
 local iconst = use"Item".newOf
 Equipment.new = Constructor(Equipment, {
     slots = {
-        Head = iconst("NullItem"),
-        Neck = iconst("NullItem"),
+        Head = iconst"NullItem",
+        Neck = iconst"NullItem",
         --Shoulders = nil,
-        Back = iconst("NullItem"),
-        Chest = iconst("NullItem"),
+        Back = iconst"NullItem",
+        Chest = iconst"NullItem",
         --Waist = nil,
-        Hands = iconst("NullItem"),
+        Hands = iconst"NullItem",
         --Wrist = nil,
-        Legs = iconst("NullItem"),
-        Feet = iconst("NullItem"),
+        Legs = iconst"NullItem",
+        Feet = iconst"NullItem",
         --Tabard = nil,
-        Shirt = iconst("NullItem"),
-        Ring1 = iconst("NullItem"),
-        Ring2 = iconst("NullItem"),
-        Trinket1 = iconst("NullItem"),
+        Shirt = iconst"NullItem",
+        Ring1 = iconst"NullItem",
+        Ring2 = iconst"NullItem",
+        Trinket1 = iconst"NullItem",
         --Trinket2 = nil,
-        MainHand = iconst("NullItem"),
-        OffHand = iconst("NullItem")
+        MainHand = iconst"NullItem",
+        OffHand = iconst"NullItem",
     }
 })
 
